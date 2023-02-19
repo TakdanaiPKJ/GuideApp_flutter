59d47f284f1b63948d1955c63c4ee46f543ed2df
S0u_Lsad <tagdanai85@gmail.com>
2023-02-19 19:06:34 +0700

    add-map

--- lib/userpage/usermap.dart
+++ lib/userpage/usermap.dart
@@ -1,12 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
-class UserMap extends StatelessWidget {
+class UserMap extends StatefulWidget {
   const UserMap({super.key});
 
   @override
+  State<UserMap> createState() => _UserMapState();
+}
+
+class _UserMapState extends State<UserMap> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Map')),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Map'),
+      ),
+      body: FlutterMap(
+        options: MapOptions(
+          center: LatLng(16, 100),
+          zoom: 9.2,
+        ),
+        nonRotatedChildren: [
+          AttributionWidget.defaultWidget(
+            source: 'OpenStreetMap contributors',
+            onSourceTapped: null,
+          ),
+        ],
+        children: [
+          TileLayer(
+            urlTemplate:
+                'https://api.mapbox.com/styles/v1/prxmz/clebbhxgy000501pq0y4l0088/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicHJ4bXoiLCJhIjoiY2xlYmIxaXd0MTdnejN2cjFxd2huZHQ0MiJ9.M_UA7jSW68pQGvr2Mbz8DQ',
+            additionalOptions: const {
+              'AccessToken': 'generate access toekn',
+              'id': 'mapbox.mapbox-streets-v8'
+            },
+          ),
+        ],
+      ),
     );
   }
 }
